e5b46e492e8086b2f0a81a25be72d23428034ebc
Hubert <hubert.pt.ka@gmail.com>
2018-04-06 17:09:21 -0400

    Initial Commit

---
+++ TableView Demo 1/TableViewController.swift
@@ -0,0 +1,129 @@
+//
+//  TableViewController.swift
+//  Scuba2
+//
+//  Created by Hubert Ka on 2018-01-11.
+//  Copyright © 2018 Hubert Ka. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+
+    //MARK: Properties
+    @IBOutlet var labels: [UILabel]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        updateWidthsForLabels(labels: labels)
+        
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    /*override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */*/
+
+    //MARK: Private methods
+    
+    // Calculate a label's width
+    private func calculateLabelWidth(label: UILabel) -> CGFloat {
+        let labelSize = label.sizeThatFits(CGSize(width: CGFloat.greatestFiniteMagnitude, height: label.frame.height))
+        return labelSize.width
+    }
+    
+    // Find maximum width of labels
+    private func calculateMaxLabelWidth(labels: [UILabel]) -> CGFloat {
+        let labelWidths = labels.map(calculateLabelWidth)
+        let maxLabelWidth = labelWidths.reduce(CGFloat.leastNormalMagnitude, { max($0, $1) })
+        return maxLabelWidth
+    }
+    
+    // Add Auto Layout constrains to all label widths
+    private func updateWidthsForLabels(labels: [UILabel]) {
+        let maxLabelWidth = calculateMaxLabelWidth(labels: labels)
+        for label in labels {
+            let constraint = NSLayoutConstraint(item: label,
+                                                attribute: .width,
+                                                relatedBy: .equal,
+                                                toItem: nil,
+                                                attribute: .notAnAttribute,
+                                                multiplier: 1,
+                                                constant: maxLabelWidth)
+            label.addConstraint(constraint)
+        }
+    }
+}
